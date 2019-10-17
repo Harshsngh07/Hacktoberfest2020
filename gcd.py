3b523298ec78bbf46f56e541b25887e28ac97d2f
RIZWAN AHMAD <rizwansoaib@gmail.com>
2019-10-17 19:40:04 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,3 +1,27 @@
+'''
+A python program to find gcd of two number.
+
+GCD=greatest common divisor 
+a=30
+b=2
+30=1*2*3*5
+2=1*2
+
+GCD=multiplication of common factors
+   =1*2
+   =2
+
+gcd1 is based on euclidean algorithm.
+
+'''
+
+
+
+
+
+
+
+
 def gcd1(a,b):
   if not a:return b
   return gcd1(b%a,a)
