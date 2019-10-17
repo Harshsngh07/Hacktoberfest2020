35e9958a7af2558bc47a974677ef6ff94a037e95
Abhishekkr3003 <abhishekkr3003@gmail.com>
2019-10-17 16:41:00 +0530

    Add Comment to the existing program of pascal triangle

--- pascal triangle.c
+++ pascal triangle.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+ //This program show the pascal triangle that we have learned in our previous classes just by entering the number of rows
+ #include <stdio.h>
 void main()
 {
     int array[15][15], i, j, rows, num = 25, k;
 
     printf("\n enter the number of rows:");
-    scanf("%d", &rows);
+    scanf("%d", &rows);                     //Enter The number of rows
     for (i = 0; i < rows; i++)
     {
         for (k = num - 2 * i; k >= 0; k--)
@@ -17,7 +18,7 @@ void main()
             }
             else
             {
-                array[i][j] = array[i - 1][j - 1] + array[i - 1][j];
+                array[i][j] = array[i - 1][j - 1] + array[i - 1][j];                //calculating the middle elements of the pascal triangle
             }
             printf("%4d", array[i][j]);
         }
