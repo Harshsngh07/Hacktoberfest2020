906b5d2308a26204f2e43ce2e193b9579788fdde
asadnaufal <asadnaufall@gmail.com>
2019-10-19 15:27:58 +0700

    add comment

--- Language/Rust/fizz_buzz.rs
+++ Language/Rust/fizz_buzz.rs
@@ -1,12 +1,17 @@
-// basic method fizzbuzz
+// basic method fizzbuzz.
 
 fn fizz_buzz(number: u32) -> () {
 
     for i in 1..number + 1 {
+        // if number is divisible by both 5 and 3, print "FizzBuzz"
 		if i % 3 == 0 && i % 5 == 0 {
 			println!("FizzBuzz");
+            
+        // if number is divisible by 3, print "Fizz"
 		} else if i % 3 == 0 {
 			println!("Fizz");
+
+        // if number is divisible by 5, print "Buzz"
 		} else if i % 5 == 0 {
 			println!("Buzz");
 		}
