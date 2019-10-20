e44d99d92c68b43ae6c41d5640a9dede1d437fa9
Axhat <55556128+Axhat@users.noreply.github.com>
2019-10-20 22:17:13 +0530

    plz add this asap

---
+++ using while,print hello n times.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+  int count=1,n;
+  /*here we have defined the count which increase as the ++ operator increases as comparable to the value of n*/
+  scanf("%d",&n);
+  while(count<=n)
+   {
+     printf("hello world \n");
+     count++;
+}
+}
---
+++ ww.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+	int sum;
+	int n,r,i,j;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	r=n % j;
+	j=j/10;
+
+	}
+	while(i<=n)
+	{
+	   sum=sum+r;
+	}
+	printf("%d",sum);
+}
