730224ada8748acaa6cdc73d4bb1c9ceb941e29d
Deepansh Sachdeva <sachdeva.de@husky.neu.edu>
2019-10-23 22:24:23 -0400

    added linklist & node java utility class

---
+++ Language/Java/Node.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ * @author deepanshsachdeva
+ * 
+ * Class to implement node in  LinkList.java
+ * 
+ */
+public class Node {
+    private int value;
+    private Node next;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
