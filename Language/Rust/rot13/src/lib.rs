8849bb1646270838a1a3b999e31458cbcc516e29
Michel Martinez <michel.martinez@linx.com.br>
2019-10-29 16:34:28 -0300

    add description

--- Language/Rust/rot13/src/lib.rs
+++ Language/Rust/rot13/src/lib.rs
@@ -1,3 +1,9 @@
+// ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13)
+// is a simple letter substitution cipher that replaces a letter
+// with the 13th letter after it, in the alphabet.
+//
+// lib to use:
+
 fn cp13(c: char) -> char {
     ((c as u8) + 13) as char
 }
--- Language/Rust/rot13/tests/rot13.rs
+++ Language/Rust/rot13/tests/rot13.rs
@@ -1,3 +1,9 @@
+// ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13)
+// is a simple letter substitution cipher that replaces a letter
+// with the 13th letter after it, in the alphabet.
+//
+// examples of tests:
+
 use rot13;
 
 #[test]
