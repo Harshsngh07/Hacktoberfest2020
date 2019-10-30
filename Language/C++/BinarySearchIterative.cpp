6b8b38df9ef8ec72c3fd6a90983c2315078652c3
Ahmed Elmayyah <a.elmayyah@gmail.com>
2019-10-30 10:21:03 +0200

    Added code comments for BinarySearchIterative.cpp

--- Language/C++/BinarySearchIterative.cpp
+++ Language/C++/BinarySearchIterative.cpp
@@ -1,21 +1,35 @@
+/*Iterative binary search algorithm*/ 
+/*It is a divide and conquer algorithm that finds the
+index of an element in a sorted array in O(log(N)).*/ 
+
 #include <iostream>
 
 using namespace std;
 
+// This function returns the index of the element.
 int binarySearch(int arr[], int length, int value)
 {
     int left = 0, right = length-1, middle;
+    //Break when the left bound is bigger than the right.
     while (right >= left)
     {
+        //Get the middle of the array
         middle = (left + right)/2;
+        
+        //If the required value is smaller than the middle
         if (arr[middle] > value)
+            //Work on the left part of the array
             right = middle-1;
-
+        
+        //If the required value is bigger than the middle
         else if (arr[middle] < value)
+            //Work on the right part of the array
             left = middle+1;
-            
+        
+        //Else: the element is found
         else return middle;
     }
+    //The element doesn't exist.
     return -1;
 }
 
