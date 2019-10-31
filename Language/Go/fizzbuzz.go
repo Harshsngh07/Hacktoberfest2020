6897f6fe295b91623efb8cd42cf21b05b2bab7e4
Abhishek Gaur <abhishek.abhishekgaur.gaur@gmail.com>
2019-10-31 23:14:31 +0530

    Create fizzbuzz.go

---
+++ Language/Go/fizzbuzz.go
@@ -0,0 +1,28 @@
+// Solution to the famous fizzbuzz challange in golang
+
+package main
+
+import "fmt"
+
+func main() {
+    for i := 1; i <= 100; i++ {
+
+        if i%3 == 0 {
+            // Multiple of 3
+            fmt.Printf("fizz")
+        }
+        if i%5 == 0 {
+            // Multiple of 5
+            fmt.Printf("buzz")
+        }
+
+        if i%3 != 0 && i%5 != 0 {
+            // Neither, so print the number itself
+            fmt.Printf("%d", i)
+        }
+
+        // A trailing new line (so both fizz + buzz can be printed on the same line)
+        fmt.Printf("\n")
+
+    }
+}
