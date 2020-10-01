a2591db0b4cc69c38ad9fc02d0ea10be4fa27d3a
soumy47 <19btcse47@suiit.ac.in>
2020-10-01 11:20:11 +0530

    added perfectNo

---
+++ Language/C/PerfectNumber.c
@@ -0,0 +1,36 @@
+//Program to check whether a no is a perfect no. or not 
+
+#include<stdio.h> 
+
+int isPerfect(int n)
+{
+    int i,r=0;
+    for(i = 1; i <= n/2; i++)
+    {
+        if(n%i==0)
+        {
+            r=r+i;
+        }  
+    }
+    if(n==r)
+        return 1;
+    return 0;
+
+}
+
+int main()
+{
+   
+    int n;
+    printf("Enter a Number: ");
+    scanf("%d",&n);
+    if(isPerfect(n)==1)
+    {
+        printf("%d is a perfect no.\n",n);
+    }
+    else
+    {
+         printf("%d is not a perfect no.\n",n);
+    }
+    return 0;
+}
