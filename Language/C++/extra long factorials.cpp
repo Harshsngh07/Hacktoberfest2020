3ece97b04ee859d66470667bf25aa9541aeb68fc
Sukanya Bharati <64637631+BharatiSukanya@users.noreply.github.com>
2020-10-09 12:08:02 +0530

    Update extra long factorials.cpp

--- Language/C++/extra long factorials.cpp
+++ Language/C++/extra long factorials.cpp
@@ -1,4 +1,9 @@
-This is a program to find the factorial of a very large number . 
+This is a program to find the factorial of a very large number.
+What is  a factorial of a number?
+It is the product of all positive integers less than or equal to 
+a given positive integer and denoted by that integer and an exclamation point. 
+For example :
+Thus, factorial seven is written 7!, meaning 1 × 2 × 3 × 4 × 5 × 6 × 7.
 
 
 #include<bits/stdc++.h> 
