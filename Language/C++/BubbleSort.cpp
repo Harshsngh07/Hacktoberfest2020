8ab465a0efb3f2362e2ae9e409979e6de8289463
Vaishnavi <vaishnavijain999@gmail.com>
2020-10-16 01:02:43 +0530

    Bubble sort in cpp

--- Language/C++/BubbleSort.cpp
+++ Language/C++/BubbleSort.cpp
@@ -1,3 +1,4 @@
+//Bubble Sort in CPP (arrange array elements in ascending order)
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -11,12 +12,12 @@ void bubbleSort(int a[], int n)
         {
             if (a[i] > a[i + 1])
             {
-                swap(a[i], a[i + 1]);
+                swap(a[i], a[i + 1]); //swap elements if a[i] is greater than a[i+1]
                 flag = 0;
             }
         }
         if (flag == 1)
-            return;
+            return; // return if no swapping is done in a round
     }
 }
 int main()
@@ -28,7 +29,7 @@ int main()
     cout << "Enter elements of array";
     for (int i = 0; i < n; i++)
         cin >> a[i];
-    bubbleSort(a, n);
+    bubbleSort(a, n); //Function calling
     for (int i = 0; i < n; i++)
-        cout << a[i] << " ";
+        cout << a[i] << " "; //Print array after sorting
 }
