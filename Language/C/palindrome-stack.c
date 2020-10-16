9b8765f0d8ad82198ff5544f2bf32729f4203479
Shubham kumar jayswal <sk662516@gmail.com>
2020-10-16 09:19:23 +0530

    added stack operations and some applications

---
+++ Language/C/palindrome-stack.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+struct ArrayStack{
+    int top;
+    char array[100];
+};
+
+void main(){
+    struct ArrayStack stack;
+    char str[100];
+    int i=0;
+    printf("Enter the string=");
+    gets(str);
+    int len=strlen(str);
+    stack.top=-1;
+    for(i=0;i<len/2;i++){
+        stack.top+=1;
+        stack.array[stack.top]=str[i];
+    }
+    int last;
+    last=len%2==0?len/2:(len/2)+1;
+    for(i=last;i<len;i++){
+        if(str[i]==stack.array[stack.top]){
+            stack.top-=1;
+            continue;
+        }
+        else{
+            printf("Not a palindrome");
+            return;
+        }
+    }
+    printf("Palindrome");
+}
---
+++ Language/C/stack.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+
+struct ArrayStack{
+    int top,capacity,array[100];
+};
+
+void main(){
+    struct ArrayStack stack;
+    printf("Enter the capacity of stack(1-100)=");
+    scanf("%d",&stack.capacity);
+    stack.top=-1;
+    int choice,item;
+    while(1){
+        printf("\n1.push\n2.pop\n3.peek\n4.isFull\n5.isEmpty\n6.exit\n");
+        printf("\nEnter your choice= ");
+        scanf("%d",&choice);
+        switch(choice){
+        case 1:
+            if(stack.top==(stack.capacity-1))
+                printf("OverFlow");
+
+            else{
+                stack.top+=1;
+                printf("\nEnter the value=");
+                scanf("%d",&stack.array[stack.top]);
+                fflush(stdin);
+                }
+            break;
+
+        case 2:
+            if(stack.top==-1)
+                printf("UnderFlow");
+            else{
+                item=stack.array[stack.top];
+                stack.top-=1;
+                printf("\nPopped value is %d",item);
+                }
+            break;
+
+        case 3:
+            if(stack.top==-1)
+                printf("\nThere is no data to peek.");
+            else{
+                item=stack.array[stack.top];
+                printf("\ntop value is %d",item);
+                }
+            break;
+
+        case 4:
+            if(stack.top==(stack.capacity-1))
+                printf("\nStack is Full");
+
+            else
+                printf("\nStack is not Full");
+            break;
+
+        case 5:
+            if(stack.top==-1)
+                printf("\nStack is empty");
+            else
+                printf("\nStack is not empty");
+            break;
+
+        case 6:
+            return;
+
+        default:
+            printf("invalid choice");
+            break;
+        }
+    }
+}
