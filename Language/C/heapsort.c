0318a7cdb613a8bc8d365f07bc2724a5069f7f40
Rico45-Vizil <61464514+Rico45-Vizil@users.noreply.github.com>
2020-10-18 11:07:57 +0530

    Update heapsort.c

--- Language/C/heapsort.c
+++ Language/C/heapsort.c
@@ -1,5 +1,9 @@
+/*Heap sort is a comparison based sorting technique based on Binary Heap data structure. 
+It is similar to selection sort where we first find the maximum element and place the maximum element at the end. We repeat the same process for the remaining elements*/
 #include<stdio.h>  
 int temp;  
+/*To heapify a subtree rooted with node i which is 
+an index in arr[]. n is size of heap*/
 void heapify(int arr[], int size, int i)  
 {  
 int largest = i;    
@@ -7,7 +11,6 @@ int left = 2*i + 1;
 int right = 2*i + 2;    
 if (left < size && arr[left] >arr[largest])  
 largest = left;  
-
 if (right < size && arr[right] > arr[largest])  
 largest = right;  
 
@@ -37,9 +40,7 @@ void main()
 int arr[] = {1, 10, 2, 3, 4, 1, 2, 100,23, 2};  
 int i;  
 int size = sizeof(arr)/sizeof(arr[0]);  
-
 heapSort(arr, size);  
-
 printf("printing sorted elements\n");  
 for (i=0; i<size; ++i)  
 printf("%d\n",arr[i]);  
