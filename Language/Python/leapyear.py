9a44c4c1afb38a51824c668b67d3ae970bef6385
GarimaDubey30 <73238370+GarimaDubey30@users.noreply.github.com>
2020-10-21 19:55:59 +0530

    Update leapyear.py

--- Language/Python/leapyear.py
+++ Language/Python/leapyear.py
@@ -1,12 +1,7 @@
 __author__ = 'Shahbaz_Akhtar'
+''' this prog tell wheather the entered year is a leap year or not
 year=int(input("Enter a year : "))
-if(year % 4 ==0):
-    if(year % 100 ==0):
-        if(year % 400 ==0):
-            print("leap year")
-        else:
-            print("Not a leap year")
-    else:
-        print("leap year")
+if((year % 4 ==0 or year % 400 ==0) and year % 100 !=0):
+    print("leap year")
 else:
     print("not a leap year")
